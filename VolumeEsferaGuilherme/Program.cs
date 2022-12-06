6af95b55e314c2396e1535f378d6f96d4d3b19a6
Lenovo <Lenovo@DESKTOP-0TVM66C>
2022-12-05 21:06:15 -0300

    Adicionar arquivos de projeto.

---
+++ VolumeEsferaGuilherme/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+namespace VolumeEsferaGuilherme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, volume;
+            Console.WriteLine("PROGRAMA PARA CALCULAR O VOLUME DE UMA ESFERA\n");
+            Console.Write("informe o raio (r) da esfera: ");
+            raio = double.Parse(Console.ReadLine());
+            if (raio < 0)
+            {
+                Console.WriteLine("\nNÃO EXISTE VOLUME NEGATIVO!");
+            }
+            else
+            {
+                volume = (4 / 3.0) * Math.PI * Math.Pow(raio, 3);
+
+                Console.WriteLine("\nO VOLUME DA ESFERA É: " + volume.ToString("F3"));
+            }
+        }
+    }
+}
